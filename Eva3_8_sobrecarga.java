ce624bac3d576469467bcea1786edd7c656fc0ab
azultorres6 <L25550863@chihuahua2.tecnm.mx>
2025-12-02 19:18:51 -0600

    Add files via upload

---
+++ Eva3_8_sobrecarga.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_8_sobrecarga;
+
+/**
+ *
+ * @author Azul
+ */
+public class Eva3_8_sobrecarga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("HOLA");
+        System.out.println(100);
+        System.out.println(100.5);
+        System.out.println(true);
+        System.out.println('c');
+        System.out.println(args);
+
+
+    }
+    
+}
+    
+    
+
